0af528d1896e56dfe5e03310cdb2e08f5a9f86cc
YuSeungYeol <sr.0220a@gmail.com>
2024-10-15 20:19:01 +0900

    [유승열] 결재 관련 수정

--- group_ware/src/main/java/com/ware/spring/authOvertime/controller/AuthOvertimeApiController.java
+++ group_ware/src/main/java/com/ware/spring/authOvertime/controller/AuthOvertimeApiController.java
@@ -1,5 +1,5 @@
 package com.ware.spring.authOvertime.controller;
-
+ 
 import java.util.HashMap;
 import java.util.Map;
 
--- group_ware/src/main/java/com/ware/spring/authorization/domain/AuthorizationDto.java
+++ group_ware/src/main/java/com/ware/spring/authorization/domain/AuthorizationDto.java
@@ -23,7 +23,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
- 
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
